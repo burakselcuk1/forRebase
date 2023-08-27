0363244c7940fddd4114bc40cce58a204cc78752
burakselcuk1 <design.burakk@gmail.com>
2023-08-27 14:41:20 +0300

    initial commit

--- app/src/main/java/com/basew/forrebase/MainActivity.kt
+++ app/src/main/java/com/basew/forrebase/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // İlk versiyon kodu
-        println("Merhaba, Dünya!")
+        println("Selam Android!")
     }
 }
