2503fcfddcf69dc23388625e0f6323c8d4845b02
burakselcuk1 <design.burakk@gmail.com>
2023-08-27 14:40:42 +0300

    Initial commit

---
+++ app/src/main/java/com/basew/forrebase/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.basew.forrebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // İlk versiyon kodu
+        println("Merhaba, Dünya!")
+    }
+}
